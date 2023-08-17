2d8975ee3004d66c13ae414e280ae22ba04af931
kalkM <yidreslekali@gmail.com>
2023-08-18 00:40:58 +0300

    Adding link to github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <br />
+      <a
+        href="https://github.com/KalkM/React-weather"
+        target="_blank"
+        rel="noreferrer"
+      >
+        open source code
+      </a>{" "}
+      by Kalkidan Mulugeta Diro
     </div>
   );
 }
